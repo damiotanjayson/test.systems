8ecc7b53e50e1931f54c90ec27786c8089717a0b
jcuanag29 <jcuanag29@gmail.com>
2020-06-13 16:28:41 +0800

    Final Commit

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'systemintegration');
+define('DB_NAME', 'system_integ_2r3');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
